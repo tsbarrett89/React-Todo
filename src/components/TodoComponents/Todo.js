dc568b56ea6609d2e5a7f1f8805cd96e49030ef7
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-29 09:57:36 -0600

    finished MVP except line-through

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./Todo.css"
+
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div className={(this.props.todo.completed === true) ? "underline" : null} onClick={() => this.props.isCompleted(this.props.todo.id)}>
+               {this.props.todo.task} 
+            </div>
+        )
+    }
+}
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+
+import React, { Component } from 'react'
+import Todo from "./Todo"
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <>
+                {this.props.todos.map(todo=>(<Todo isCompleted={this.props.isCompleted} todo={todo} key={todo.id}></Todo>))}
+            </>
+        )
+    }
+}
